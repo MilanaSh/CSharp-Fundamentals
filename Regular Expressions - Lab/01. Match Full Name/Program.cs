75ac20ef9e6c22ef09091fbb6843aa91db11dff4
Milana Shengarova <90791831+MilanaSh@users.noreply.github.com>
2022-05-09 00:16:08 +0300

    Add files via upload

---
+++ Regular Expressions - Lab/01. Match Full Name/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01._Match_Full_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string names = Console.ReadLine();
+
+            Regex regex = new Regex(@"\b[A-Z][a-z]+ [A-Z][a-z]+");
+
+            MatchCollection correctOnce = regex.Matches(names);
+
+            foreach (Match item in correctOnce)
+            {
+                Console.Write(item.Value + " ");
+            }
+        }
+    }
+}
---
+++ Regular Expressions - Lab/02. Match Phone Number/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _02._Match_Phone_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Regex regex = new Regex(@"\+([359]{3})( ?-?)2(\2)([0-9]{3})(\2)([0-9]{4})\b");
+
+            MatchCollection phoneMatches = regex.Matches(input);
+            List<string> phoneList = new List<string>();
+
+            foreach (Match phone in phoneMatches)
+            {
+                phoneList.Add(phone.ToString());
+            }
+            Console.WriteLine(String.Join(", ",phoneList));
+        }
+    }
+}
