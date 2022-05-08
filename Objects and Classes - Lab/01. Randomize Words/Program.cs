f18b6ad511941a730324e20cbeb8b8c8d166a652
Milana Shengarova <90791831+MilanaSh@users.noreply.github.com>
2022-05-09 00:14:22 +0300

    Add files via upload

---
+++ Objects and Classes - Lab/01. Randomize Words/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _01._Randomize_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                int position = rnd.Next(words.Length);
+
+                string temp = words[i];
+                words[i] = words[position];
+                words[position] = temp;
+
+            }
+
+            Console.WriteLine(String.Join(Environment.NewLine, words));
+        }
+    }
+}
---
+++ Objects and Classes - Lab/04. Students/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            string input = Console.ReadLine();
+
+            while (input!= "end")
+            {
+                string[] command = input.Split();
+
+                string firstName = command[0];
+                string lastName = command[1];
+                int age = int.Parse(command[2]);
+                string city = command[3];
+
+                Student student = new Student()
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Age = age,
+                    City = city
+                };
+
+                students.Add(student);
+
+                input = Console.ReadLine();
+            }
+
+            string filtredCity = Console.ReadLine();
+
+            List<Student> filtredStudents = students
+                .Where(x => x.City == filtredCity)
+                .ToList();
+
+            foreach (Student student in filtredStudents)
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+            }
+
+        }
+
+
+        public class Student
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public int Age { get; set; }
+            public string City { get; set; }
+
+        }
+
+    }
+}
